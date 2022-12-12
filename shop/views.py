e09a6eced5e87f98131f2c200e9f216acf4f0978
Andrey <andreeey.gg@gmail.com>
2022-12-12 20:28:32 +0100

    change models and add functionality

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()}
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'products/details.html', {'product': product})
+
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'products/category.html', {'category': category, 'products': products})
